f5dad26a34f5cad90aa7940b2dbdd3e8b2c21047
Joe Pecsi <jpecsi@me.com>
2014-06-26 15:30:06 -0400

    Added keyword functionality (almost)

--- main.js
+++ main.js
@@ -6,37 +6,64 @@ define(function (require, exports, module) {
         LanguageManager = brackets.getModule("language/LanguageManager");
     
     
+    function log(s) {
+        console.log("[PS-DevKit] "+s);
+    }
+    
+    
+    function keyWordCheck(st){
+        
+        var psKeywords = ["For", "If", "Add-PSSnapin", "While", "-lt", "-eq"];
+        var stat = "";
+        for (var i = 0; i < psKeywords.length; i++){
+            if (st.match(psKeywords[i])){
+                stat = true;
+            } else {
+                stat = false;
+            }
+        log(psKeywords[i]);
+        return stat;
+        }
+    }
+    
     //Define PowerShell
     CodeMirror.defineMode("powershell", function() {
-        return{
+    return {
+        startState: function() {return {inString: false};},
+        token: function(stream, state) {
             
-        startStat: function() {return {inString: false};},
-        token: function(stream, state){
-            //If a string starts here
-            if (!state.inString && stream.peek() == '"'){
-                stream.next();              //Skip quote
-                state.inString = true;      //Update state
+            // If a string starts here
+            if (!state.inString && stream.peek() == '"') { 
+                stream.next();            // Skip quote
+                state.inString = true;    // Update state     
+            }
+            
+            
+            if (!state.inString && (keyWordCheck(stream))){
+        
+                stream.next();
+                return "keyword";
+                
             }
             
             if (state.inString) {
                 
-                if (stream.skipTo('"')){    //Quote found on this line
-                    stream.next();          //Skip quote
-                    state.inString=false;   //Clear flag
+                if (stream.skipTo('"')) { // Quote found on this line
+                    stream.next();          // Skip quote
+                    state.inString = false; // Clear flag
                 } else {
-                    stream.skipToEnd();     //Rest of line is string
+                    stream.skipToEnd();    // Rest of line is string
                 }
                 
-                return "red-text";          //Token style
-                
+                return "string";          // Token style
+            
             } else {
-                
                 stream.skipTo('"') || stream.skipToEnd();
-                return "red-text";          //Unstyled token
-                
-                }   
-            }  
-        };    
+                return null;              // Unstyled token
+            }
+        
+       }
+    };
     });
     
     
@@ -47,10 +74,6 @@ define(function (require, exports, module) {
     fileExtensions: ["ps1"],
     lineComment: ["\/\/"]
     });
-    
-    function log(s) {
-            console.log("[PS-DevKit] "+s);
-    }
 
     log("The PowerShell extension has loaded!");
 
